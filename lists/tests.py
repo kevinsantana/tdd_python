63a22f877d10b203b6239e42fd52c97fd36f3a9b
Kevin Santana <kevin_santana.araujo@hotmail.com>
2020-07-21 22:50:26 -0300

    Template na view da home page

--- lists/tests.py
+++ lists/tests.py
@@ -1,19 +1,8 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
-from django.template.loader import render_to_string
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get("/")
-
-        html = response.content.decode("utf-8")        
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.strip().endswith("</html>"))
-
         self.assertTemplateUsed(response, "home.html")
